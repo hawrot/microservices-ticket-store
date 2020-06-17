0a84c5dc3204ad7fec285889be40abd449788d87
hawrot <mhawro200@caledonian.ac.uk>
2020-06-17 23:59:04 +0100

    Implement custom error in the middleware

--- auth/src/middlewares/error-handler.ts
+++ auth/src/middlewares/error-handler.ts
@@ -1,14 +1,11 @@
 import express, {Response, Request, NextFunction} from 'express';
-import {RequestValidationError} from "../errors/request-validation-error";
-import {DatabaseConnectionError} from "../errors/database-connection-error";
+import {CustomErr} from "../errors/custom-err";
 
 export const errorHandler = (err: Error, req: Request, res: Response, next: NextFunction) => {
-   if(err instanceof RequestValidationError){
+   if(err instanceof CustomErr){
        return res.status(err.statusCode).send({errors: err.serializeErrors()});
    }
 
-   if(err instanceof DatabaseConnectionError){
-       return res.status(err.statusCode).send({errors: err.serializeErrors()});
-}
+
    res.status(400).send({errors: [{message: 'Something went wrong!'}]});
 };
