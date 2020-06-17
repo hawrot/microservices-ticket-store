2d9b5ca19ddfc603a59c2e6222c117a1b141a0e9
hawrot <mhawro200@caledonian.ac.uk>
2020-06-17 22:24:22 +0100

    Create error handler

---
+++ auth/src/middlewares/error-handler.ts
@@ -0,0 +1,7 @@
+import express, {Response, Request, NextFunction} from 'express';
+
+export const errorHandler = (err: Error, req: Request, res: Response, next: NextFunction) => {
+  console.log('Sowmething went wromg', err);
+
+  res.status(400).send('Error occurred');
+};
