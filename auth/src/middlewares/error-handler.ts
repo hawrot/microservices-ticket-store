eccb9cea0617de400bb1f0ce74d691ccfd2979bb
hawrot <mhawro200@caledonian.ac.uk>
2020-06-17 23:28:05 +0100

    Convert error response for RequestValErrors

--- auth/src/middlewares/error-handler.ts
+++ auth/src/middlewares/error-handler.ts
@@ -1,7 +1,19 @@
 import express, {Response, Request, NextFunction} from 'express';
+import {RequestValidationError} from "../errors/request-validation-error";
+import {DatabaseConnectionError} from "../errors/database-connection-error";
 
 export const errorHandler = (err: Error, req: Request, res: Response, next: NextFunction) => {
-  console.log('Sowmething went wromg', err);
+   if(err instanceof RequestValidationError){
+       const formattedErrors = err.errors.map(error => {
+           return {message: error.msg, field: error.param};
+       });
 
-  res.status(400).send({message: err.message});
+       return res.status(400).send({errors: formattedErrors});
+   }
+
+   if(err instanceof DatabaseConnectionError){
+       console.log('handling this error as a db connection err')
+}
+
+    res.status(400).send({message: err.message});
 };
