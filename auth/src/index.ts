eaef5532312400f912246807c65d4963e0dec160
hawrot <mhawro200@caledonian.ac.uk>
2020-06-17 22:31:29 +0100

    Pass error message to middleware

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -5,6 +5,7 @@ import { currentUserRouter } from "./routes/current-user";
 import { signInRouter} from "./routes/signin";
 import { signOutRouter} from "./routes/signout";
 import { signUpRouter} from "./routes/signup";
+import {errorHandler} from "./middlewares/error-handler";
 
 const app = express();
 app.use(json());
@@ -13,6 +14,7 @@ app.use(currentUserRouter);
 app.use(signInRouter);
 app.use(signOutRouter);
 app.use(signUpRouter);
+app.use(errorHandler);
 
 app.listen(3000, ()=>{
     console.log('[Auth Service] - Listening on port 3000 ');
--- auth/src/middlewares/error-handler.ts
+++ auth/src/middlewares/error-handler.ts
@@ -3,5 +3,5 @@ import express, {Response, Request, NextFunction} from 'express';
 export const errorHandler = (err: Error, req: Request, res: Response, next: NextFunction) => {
   console.log('Sowmething went wromg', err);
 
-  res.status(400).send('Error occurred');
+  res.status(400).send({message: err.message});
 };
