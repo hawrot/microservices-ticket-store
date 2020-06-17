b8edf4c7f8b4ae0559903f94171be066d313a5ce
hawrot <mhawro200@caledonian.ac.uk>
2020-06-18 00:09:23 +0100

    Add route error handler to index.ts

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -6,6 +6,7 @@ import { signInRouter} from "./routes/signin";
 import { signOutRouter} from "./routes/signout";
 import { signUpRouter} from "./routes/signup";
 import {errorHandler} from "./middlewares/error-handler";
+import {NotFoundError} from "./errors/not-found-error";
 
 const app = express();
 app.use(json());
@@ -14,6 +15,12 @@ app.use(currentUserRouter);
 app.use(signInRouter);
 app.use(signOutRouter);
 app.use(signUpRouter);
+
+//Throw error on wrong route
+app.get('*', () => {
+    throw new NotFoundError()
+})
+
 app.use(errorHandler);
 
 app.listen(3000, ()=>{
