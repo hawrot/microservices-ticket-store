57a75c606d779f68b8a3b2c58bbf97c9f097dc1a
hawrot <mhawro200@caledonian.ac.uk>
2020-06-19 15:50:57 +0100

    Pre-save password hooks

--- auth/src/models/users.ts
+++ auth/src/models/users.ts
@@ -1,4 +1,5 @@
 import mongoose from 'mongoose';
+import {Password} from "../services/password";
 
 // An interface that describes the properties that are required for user creation
 interface UserAttrs {
@@ -7,12 +8,12 @@ interface UserAttrs {
 }
 
 //An interface that describes the properties that the User model has
-interface UserModel extends mongoose.Model<UserDoc>{
+interface UserModel extends mongoose.Model<UserDoc> {
     build(attrs: UserAttrs): UserDoc;
 }
 
 //An interface that describes the properties that a User Document has
-interface UserDoc extends mongoose.Document{
+interface UserDoc extends mongoose.Document {
     email: string;
     password: string;
 }
@@ -29,10 +30,18 @@ const userSchema = new mongoose.Schema({
     }
 });
 
+userSchema.pre('save', async function (done) {
+    if (this.isModified('password')) {
+        const hashed = await Password.toHash(this.get('password'));
+        this.set('password', hashed);
+    }
+    done();
+})
+
 userSchema.statics.build = (attrs: UserAttrs) => {
     return new User(attrs);
 }
 
 const User = mongoose.model<UserDoc, UserModel>('User', userSchema);
 
-export { User };
+export {User};
