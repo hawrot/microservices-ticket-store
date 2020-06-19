5178c1863f9747ddb3dbb5ab3c1a39c1234e1c99
hawrot <mhawro200@caledonian.ac.uk>
2020-06-19 15:36:44 +0100

    Add password hashing function

---
+++ auth/src/services/password.ts
@@ -0,0 +1,22 @@
+import {scrypt, randomBytes} from 'crypto';
+import {promisify} from 'util';
+import {raw} from "express";
+
+const scryptAsync = promisify(scrypt);
+
+export class Password {
+
+    static async toHash(password: string) {
+        const salt = randomBytes(8).toString('hex');
+        const buf = (await scryptAsync(password, salt, 64)) as Buffer;
+
+        return `${buf.toString('hex')}.${salt}`;
+
+
+    }
+
+    static compare(storedPassword: string, suppliedPassword: string) {
+
+    }
+
+}
