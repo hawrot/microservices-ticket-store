48d89c2b8a8b4e7271d1305e134ba23be0db8200
hawrot <mhawro200@caledonian.ac.uk>
2020-06-19 12:52:25 +0100

    Add attributions check in User model

--- auth/src/models/users.ts
+++ auth/src/models/users.ts
@@ -1,5 +1,12 @@
 import mongoose from 'mongoose';
 
+// An interface that describes the properties that are required for user creation
+
+interface UserAttrs {
+    email: string;
+    password: string;
+}
+
 const userSchema = new mongoose.Schema({
     email: {
         type: String,
@@ -13,4 +20,9 @@ const userSchema = new mongoose.Schema({
 
 const User = mongoose.model('User', userSchema);
 
-export {User};
+const buildUser = (attrs: UserAttrs) => {
+    return new User(attrs);
+}
+
+
+export { User };
