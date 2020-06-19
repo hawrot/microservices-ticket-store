4764a5ed033d0dbe3857f42b16222210ff52626f
hawrot <mhawro200@caledonian.ac.uk>
2020-06-19 14:47:00 +0100

    Improve the User model

--- auth/src/models/users.ts
+++ auth/src/models/users.ts
@@ -7,6 +7,11 @@ interface UserAttrs {
     password: string;
 }
 
+//An interface that describes the properties that the User model has
+interface UserModel extends mongoose.Model<any>{
+    build(attrs: UserAttrs): any
+}
+
 const userSchema = new mongoose.Schema({
     email: {
         type: String,
@@ -18,11 +23,12 @@ const userSchema = new mongoose.Schema({
     }
 });
 
-const User = mongoose.model('User', userSchema);
-
-const buildUser = (attrs: UserAttrs) => {
+userSchema.statics.build = (attrs: UserAttrs) => {
     return new User(attrs);
 }
+
+const User = mongoose.model<any, UserModel>('User', userSchema);
+
 
 
 export { User };
