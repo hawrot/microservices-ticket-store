365b902ae222917a762c83577bb736ba799cd597
hawrot <mhawro200@caledonian.ac.uk>
2020-06-20 22:21:15 +0100

    Add validity check to sigin

--- auth/src/routes/signin.ts
+++ auth/src/routes/signin.ts
@@ -1,9 +1,19 @@
-import express from 'express';
+import express, {Request, Response} from 'express';
+import { body } from "express-validator";
 
 const router = express.Router();
 
-router.post('/api/users/signin', (req, res) =>{
-    res.send('Sign in router');
+router.post('/api/users/signin', [
+    body('email')
+        .isEmail()
+        .withMessage('Email must be valid'),
+    body('password')
+        .trim()
+        .notEmpty()
+        .withMessage('You must enter the password')
+], (req: Request, res: Response) =>{
+
+
 });
 
 
