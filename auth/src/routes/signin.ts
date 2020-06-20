865b1c9489a055bdb9df2544e118e22328308d17
hawrot <mhawro200@caledonian.ac.uk>
2020-06-20 22:37:21 +0100

    Generate token and store it in the cookies

--- auth/src/routes/signin.ts
+++ auth/src/routes/signin.ts
@@ -5,6 +5,7 @@ import {validateRequest} from "../middlewares/validate-request";
 import {User} from "../models/users";
 import {BadRequestErr} from "../errors/bad-request-err";
 import {Password} from "../services/password";
+import jwt from "jsonwebtoken";
 
 const router = express.Router();
 
@@ -29,6 +30,20 @@ router.post('/api/users/signin', [
         if (!passwordsMatch) {
             throw new BadRequestErr('Invalid credentials');
         }
+
+        //Generate JWT
+        const userJwt =jwt.sign({
+                id: existingUser.id,
+                email: existingUser.id
+            }, process.env.JWT_KEY!
+        );
+
+        //Store it on session object
+        req.session = {
+            jwt: userJwt
+        }
+
+        res.status(200).send(existingUser);
 
     });
 
