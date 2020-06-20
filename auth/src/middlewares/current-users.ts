a27639225adf45b42f712f6ab71c8ac2ef2f5187
hawrot <mhawro200@caledonian.ac.uk>
2020-06-20 23:04:06 +0100

    Declare global in current-users.ts

--- auth/src/middlewares/current-users.ts
+++ auth/src/middlewares/current-users.ts
@@ -6,13 +6,21 @@ interface UserPayload {
     email: string;
 }
 
+declare global{
+    namespace Express {
+        interface Request {
+            currentUser? : UserPayload
+        }
+    }
+}
+
 export const currentUser = (req: Request, res: Response, next: NextFunction) => {
     if(!req.session?.jwt){
         return next();
     }
 
     try {
-        const payload = jwt.verify(req.session.jwt, process.env.JWT_KEY!);
+        const payload = jwt.verify(req.session.jwt, process.env.JWT_KEY!) as UserPayload;
         res.currentUser = payload;
 
     }
