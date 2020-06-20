a51a40c2d4da67890a4c8aca401817f6862624bc
hawrot <mhawro200@caledonian.ac.uk>
2020-06-21 00:13:19 +0100

    Pass the JWT_KEY

--- auth/src/test/setup.ts
+++ auth/src/test/setup.ts
@@ -5,7 +5,9 @@ import {app} from "../app";
 let mongo: any;
 
 beforeAll(async () => {
-     mongo = new MongoMemoryServer();
+    process.env.JWT_KEY = 'abcdef';
+
+    mongo = new MongoMemoryServer();
     const mongoUri = await mongo.getUri();
 
     await mongoose.connect(mongoUri, {
@@ -16,15 +18,15 @@ beforeAll(async () => {
 });
 
 beforeEach(async () => {
-   const collections = await mongoose.connection.db.collections();
+    const collections = await mongoose.connection.db.collections();
 
-   //reset all the data
-    for (let collection of collections){
+    //reset all the data
+    for (let collection of collections) {
         await collection.deleteMany({});
     }
 });
 
 afterAll(async () => {
-   await mongo.stop();
-   await mongoose.connection.close();
+    await mongo.stop();
+    await mongoose.connection.close();
 });
