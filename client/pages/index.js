b97507e72b9915c8070eeadaf4d732757086d5e9
hawrot <mhawro200@caledonian.ac.uk>
2020-06-22 17:14:49 +0100

    Pass the headers

--- client/pages/index.js
+++ client/pages/index.js
@@ -2,18 +2,15 @@ import axios from 'axios';
 
 const LandingPage = ({currentUser}) => {
     console.log(currentUser)
-    console.log(currentUser);
     return <h1>Landing Page</h1>;
 
 };
 
-LandingPage.getInitialProps = async () => {
+LandingPage.getInitialProps = async ({ req }) => {
    if (typeof window === 'undefined'){
        //we are on the server
        const { data } = await axios.get('http://ingress-nginx-controller.ingress-nginx.svc.cluster.local/api/users/currentuser', {
-           headers: {
-               Host: 'ticketing.dev'
-           }
+           headers: req.headers
        });
 
    }else {
