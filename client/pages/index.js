d973a0e4797cc6c55abdf2d4ca2dfca84996b741
hawrot <mhawro200@caledonian.ac.uk>
2020-06-22 17:20:55 +0100

    Create build-client api

--- client/pages/index.js
+++ client/pages/index.js
@@ -6,18 +6,20 @@ const LandingPage = ({currentUser}) => {
 
 };
 
-LandingPage.getInitialProps = async ({ req }) => {
-   if (typeof window === 'undefined'){
-       //we are on the server
-       const { data } = await axios.get('http://ingress-nginx-controller.ingress-nginx.svc.cluster.local/api/users/currentuser', {
-           headers: req.headers
-       });
+LandingPage.getInitialProps = async ({req}) => {
+    if (typeof window === 'undefined') {
+        //we are on the server
+        const {data} = await axios.get('http://ingress-nginx-controller.ingress-nginx.svc.cluster.local/api/users/currentuser', {
+            headers: req.headers
+        });
 
-   }else {
-       const response = await axios.get('/api/users/currentuser');
-       return response.data;
-   }
-   return {};
+        return data;
+
+    } else {
+        const response = await axios.get('/api/users/currentuser');
+        return response.data;
+    }
+    return {};
 }
 
 export default LandingPage;
