925c9dbbe380636e9a33d0cdf2d9e1d9ba71b925
hawrot <mhawro200@caledonian.ac.uk>
2020-06-22 23:08:39 +0100

    Conditionally Showing Links

--- client/components/header.js
+++ client/components/header.js
@@ -1,6 +1,16 @@
 import Link from 'next/link';
 
 export default ({currentUser}) => {
+
+    const links = [
+        !currentUser && {label: 'Sign up', href: '/auth/signup'},
+        !currentUser && {label: 'Sign in', href: '/auth/signin'},
+        currentUser && {label: 'Sign out', href: '/auth/signout'},
+    ].filter(linkConfig => linkConfig)
+        .map(({label, href}) => {
+            return <li key={href}>{label}</li>
+        })
+
   return <nav className="navbar navbar-light bg-light">
       <Link href="/">
           <a className="navbar-light">GitTix</a>
@@ -8,7 +18,7 @@ export default ({currentUser}) => {
 
       <div className="d-flex justify-content-end">
           <ul className="nav d-flex align-items-center">
-              {currentUser ? 'Sign out' : 'Sign in/up'}
+              {links}
           </ul>
       </div>
   </nav>
