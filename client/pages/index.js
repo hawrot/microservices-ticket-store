d05075ed0acdb8cd286a912d2f1fc428aa8793f5
hawrot <mhawro200@caledonian.ac.uk>
2020-06-22 17:06:57 +0100

    Getting the currentuser from the srv

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,3 +1,26 @@
-export default () => {
+import axios from 'axios';
+
+const LandingPage = ({currentUser}) => {
+    console.log(currentUser)
+    console.log(currentUser);
     return <h1>Landing Page</h1>;
+
 };
+
+LandingPage.getInitialProps = async () => {
+   if (typeof window === 'undefined'){
+       //we are on the server
+       const { data } = await axios.get('http://ingress-nginx-controller.ingress-nginx.svc.cluster.local/api/users/currentuser', {
+           headers: {
+               Host: 'ticketing.dev'
+           }
+       });
+
+   }else {
+       const response = await axios.get('/api/users/currentuser');
+       return response.data;
+   }
+   return {};
+}
+
+export default LandingPage;
