2126ddf057b346351a6e3661196a2b876a4cd9d7
hawrot <mhawro200@caledonian.ac.uk>
2020-06-23 23:40:02 +0100

    Write first tests

---
+++ tickets/src/routes/__test__/new.test.ts
@@ -0,0 +1,23 @@
+import request from 'supertest';
+import {app} from "../../app";
+
+it('should have a route handler listening to /api/tickets for post requests',async function () {
+
+});
+
+it('should can only be accessed when the user is signin',async function () {
+
+});
+
+it('should return an error if an invalid title is provided',async function () {
+
+});
+
+it('should return an error if an invalid price is provided',async function () {
+
+});
+
+it('should create a ticket with valid input',async function () {
+
+});
+
