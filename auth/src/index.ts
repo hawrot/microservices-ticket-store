53db294acb65fb29d994fa3ea2ad8c0e83ddfb02
hawrot <mhawro200@caledonian.ac.uk>
2020-06-23 23:34:49 +0100

    hardcode mongo uri for auth

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -5,8 +5,13 @@ const start = async () => {
     if (!process.env.JWT_KEY){
         throw new Error('Error JWT must be defined');
     }
+    if (!process.env.MONGO_URI){
+        throw new Error('Error MONGO URI must be defined');
+    }
+
+
     try {
-        await mongoose.connect('mongodb://auth-mongo-srv:27017/auth', {
+        await mongoose.connect(process.env.MONGO_URI, {
             useNewUrlParser: true,
             useUnifiedTopology: true,
             useCreateIndex: true
