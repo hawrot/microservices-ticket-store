7678544156473b97c3e98e52c84819bd1a2e6e9d
hawrot <mhawro200@caledonian.ac.uk>
2020-06-24 00:42:48 +0100

    Configure another test

--- tickets/src/routes/__test__/new.test.ts
+++ tickets/src/routes/__test__/new.test.ts
@@ -62,6 +62,16 @@ it('should return an error if an invalid price is provided', async function () {
 });
 
 it('should create a ticket with valid input', async function () {
+    //add in a check to make sure a ticket was saved
 
+    await request(app)
+        .post('/api/tickets')
+        .set('Cookie', global.signin())
+        .send({
+            title: 'Title',
+            price: 20
+
+        })
+        .expect(201)
 });
 
