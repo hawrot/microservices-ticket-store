41d9e9da52b505d2f87e5e626a4aa63f19eb2349
hawrot <mhawro200@caledonian.ac.uk>
2020-06-23 23:14:36 +0100

    Copy dockerfile and basic config

--- tickets/src/app.ts
+++ tickets/src/app.ts
@@ -4,11 +4,6 @@ import 'express-async-errors';
 import mongoose from 'mongoose';
 import cookieSession from "cookie-session";
 
-
-import {currentUserRouter} from "./routes/current-user";
-import {signInRouter} from "./routes/signin";
-import {signOutRouter} from "./routes/signout";
-import {signUpRouter} from "./routes/signup";
 import {errorHandler} from "@mhmicrotickets/common";
 import {NotFoundError} from "@mhmicrotickets/common";
 
@@ -19,11 +14,6 @@ app.use(cookieSession({
     signed: false,
     secure: process.env.NODE_ENV !== 'test'
 }));
-
-app.use(currentUserRouter);
-app.use(signInRouter);
-app.use(signOutRouter);
-app.use(signUpRouter);
 
 //Throw error on wrong route
 app.get('*', async (req, res) => {
---
+++ tickets/src/index.ts
@@ -0,0 +1,25 @@
+import mongoose from 'mongoose';
+import { app } from "./app";
+
+const start = async () => {
+    if (!process.env.JWT_KEY){
+        throw new Error('Error JWT must be defined');
+    }
+    try {
+        await mongoose.connect('mongodb://auth-mongo-srv:27017/', {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+            useCreateIndex: true
+        });
+    } catch (e) {
+        console.log(e);
+    }
+
+    app.listen(3000, () => {
+        console.log('[Auth Service] - Listening on port 3000 ');
+    })
+}
+
+start();
+
+
