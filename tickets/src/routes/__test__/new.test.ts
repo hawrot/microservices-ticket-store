df474b45d4b4b428b44d2a6e80562542a2e37e08
hawrot <mhawro200@caledonian.ac.uk>
2020-06-24 00:41:11 +0100

    Add validation for tickets

--- tickets/src/routes/__test__/new.test.ts
+++ tickets/src/routes/__test__/new.test.ts
@@ -1,7 +1,7 @@
 import request from 'supertest';
 import {app} from "../../app";
 
-it('should have a route handler listening to /api/tickets for post requests',async function () {
+it('should have a route handler listening to /api/tickets for post requests', async function () {
     const response = await request(app)
         .post('/api/tickets')
         .send({});
@@ -9,7 +9,7 @@ it('should have a route handler listening to /api/tickets for post requests',asy
     expect(response.status).not.toEqual(404);
 });
 
-it('should can only be accessed when the user is signin',async function () {
+it('should can only be accessed when the user is signin', async function () {
     const response = await request(app)
         .post('/api/tickets')
         .send({});
@@ -26,7 +26,7 @@ it('should  return a status other than 401 if  the user is signed in', async fun
     expect(response.status).not.toEqual(401);
 });
 
-it('should return an error if an invalid title is provided',async function () {
+it('should return an error if an invalid title is provided', async function () {
     const response = await request(app)
         .post('/api/tickets')
         .set('Cookie', global.signin())
@@ -39,11 +39,29 @@ it('should return an error if an invalid title is provided',async function () {
 
 });
 
-it('should return an error if an invalid price is provided',async function () {
+it('should return an error if an invalid price is provided', async function () {
+     await request(app)
+        .post('/api/tickets')
+        .set('Cookie', global.signin())
+        .send({
+            title: 'Title',
+            price: -10
+        })
+     .expect(400)
+
+   await request(app)
+        .post('/api/tickets')
+        .set('Cookie', global.signin())
+        .send({
+            title: 'Title'
+
+        })
+       .expect(400)
+
 
 });
 
-it('should create a ticket with valid input',async function () {
+it('should create a ticket with valid input', async function () {
 
 });
 
--- tickets/src/routes/new.ts
+++ tickets/src/routes/new.ts
@@ -1,10 +1,15 @@
 import express, {Request, Response} from 'express';
-import { requireAuth} from '@mhmicrotickets/common';
+import { requireAuth, validateRequest} from '@mhmicrotickets/common';
+import { body } from 'express-validator';
 
 
 const router = express.Router();
 
-router.post('/api/tickets', requireAuth, (req: Request, res: Response) => {
+router.post('/api/tickets', requireAuth,[
+    body('title').not().isEmpty().withMessage('Title is required'),
+    body('price').isFloat({ gt: 0 }).withMessage('Price must be greater than 0')
+
+], validateRequest, (req: Request, res: Response) => {
     res.sendStatus(200);
 })
 
