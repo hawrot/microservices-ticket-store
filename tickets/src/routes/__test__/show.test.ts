14dc2b8fe3cfddaa7b574defda03acb51c775b4c
hawrot <mhawro200@caledonian.ac.uk>
2020-06-24 22:54:24 +0100

    Create test for fetching tickets

---
+++ tickets/src/routes/__test__/show.test.ts
@@ -0,0 +1,34 @@
+import request from 'supertest';
+import {app} from "../../app";
+import {Ticket} from "../../models/ticket";
+
+
+it('should return 404 if the ticket is not found', async function () {
+    await request(app)
+        .get('/api/tickets/lsdkaoskdksd')
+        .send()
+        .expect(404);
+});
+
+it('should return ticket if the ticket is available', async function () {
+    const title = 'concert';
+    const price = 20;
+
+    const response = await request(app)
+        .post('/api/tickets')
+        .set('Cookie', global.signin())
+        .send({
+            title: title,
+            price: price
+        })
+        .expect(201);
+
+    const ticketResponse = await request(app)
+        .get(`/api/tickets/${response.body.id}`)
+        .send()
+        .expect(200);
+
+    expect(ticketResponse.body.title).toEqual(title);
+    expect(ticketResponse.body.price).toEqual(price);
+
+});
