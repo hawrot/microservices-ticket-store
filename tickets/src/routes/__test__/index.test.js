6c00b494b37b9461fc6551d8b9394124798b2599
hawrot <mhawro200@caledonian.ac.uk>
2020-06-25 00:05:42 +0100

    Create index test

---
+++ tickets/src/routes/__test__/index.test.js
@@ -0,0 +1,24 @@
+import request from 'supertest';
+import {app} from "../../app";
+import {Ticket} from "../../models/ticket";
+
+const createTicket = () => {
+    request(app)
+        .post('/api/tickets')
+        .set('Cookie', global.signin())
+        .send({
+            title: 'Ticket 2',
+            price: 20
+        })
+}
+
+it('should fetch a list of tickets', async function () {
+    await createTicket();
+    await createTicket();
+    await createTicket();
+
+    const response = await request(app)
+        .get('/api/tickets')
+        .send()
+        .expect(200);
+});
