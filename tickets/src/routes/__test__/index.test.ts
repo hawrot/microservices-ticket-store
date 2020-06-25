75242b4721a754da1ac47515d3e0b970f0dc1b92
hawrot <mhawro200@caledonian.ac.uk>
2020-06-25 17:01:19 +0100

    Fix the index.test.ts

--- tickets/src/routes/__test__/index.test.ts
+++ tickets/src/routes/__test__/index.test.ts
@@ -1,26 +1,19 @@
 import request from 'supertest';
-import {app} from "../../app";
-import {Ticket} from "../../models/ticket";
+import { app } from '../../app';
 
 const createTicket = () => {
-    request(app)
-        .post('/api/tickets')
-        .set('Cookie', global.signin())
-        .send({
-            title: 'Ticket 2',
-            price: 20
-        })
-}
+    return request(app).post('/api/tickets').set('Cookie', global.signin()).send({
+        title: 'asldkf',
+        price: 20,
+    });
+};
 
-it('should fetch a list of tickets', async function () {
+it('can fetch a list of tickets', async () => {
     await createTicket();
     await createTicket();
     await createTicket();
 
-    const response = await request(app)
-        .get('/api/tickets')
-        .send()
-        .expect(200);
+    const response = await request(app).get('/api/tickets').send().expect(200);
 
     expect(response.body.length).toEqual(3);
 });
