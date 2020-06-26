4317da24fbd48b94a50ef42caefe09a1faed039f
hawrot <mhawro200@caledonian.ac.uk>
2020-06-26 22:31:17 +0100

    Close the connection

--- tickets/nats-test/src/listener.ts
+++ tickets/nats-test/src/listener.ts
@@ -6,16 +6,30 @@ const stan = nats.connect('ticketing', '123', {
     url: 'http://localhost:4222'
 });
 
+
 stan.on('connect', () => {
     console.log('Listener connected to NATS');
 
-    const subscription = stan.subscribe('ticket:created');
+    stan.on('close', () => {
+        console.log('NATS connection closed!');
+        process.exit();
+    })
+
+    const options = stan.subscriptionOptions()
+        .setManualAckMode(true)
+    const subscription = stan.subscribe('ticket:created', 'order-service-queue-group', options);
 
     subscription.on('message', (msg: Message) => {
         const data = msg.getData();
 
-        if (typeof data === 'string'){
+        if (typeof data === 'string') {
             console.log(`Recieved event #${msg.getSequence()}, with data: ${data}`)
         }
+
+        msg.ack();
+
     });
-})
+});
+
+process.on('SIGNT', () => stan.close());
+process.on('SIGTERM', () => stan.close());
