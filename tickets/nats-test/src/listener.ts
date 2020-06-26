fe49790be8fb8fc5d5fcebd05b7d4ba4f5a4b23f
hawrot <mhawro200@caledonian.ac.uk>
2020-06-27 00:16:17 +0100

    Send all available events to listener

--- tickets/nats-test/src/listener.ts
+++ tickets/nats-test/src/listener.ts
@@ -15,8 +15,7 @@ stan.on('connect', () => {
         process.exit();
     })
 
-    const options = stan.subscriptionOptions()
-        .setManualAckMode(true)
+    const options = stan.subscriptionOptions().setManualAckMode(true).setDeliverAllAvailable();
     const subscription = stan.subscribe('ticket:created', 'order-service-queue-group', options);
 
     subscription.on('message', (msg: Message) => {
