83a5b74ee59bbee041bd2c04bdbeb0d889433681
hawrot <mhawro200@caledonian.ac.uk>
2020-06-28 00:20:25 +0100

    Pass the client to the publisher

--- tickets/src/routes/new.ts
+++ tickets/src/routes/new.ts
@@ -3,6 +3,7 @@ import { requireAuth, validateRequest} from '@mhmicrotickets/common';
 import { body } from 'express-validator';
 import {Ticket} from "../models/ticket";
 import {TicketCreatedPublisher} from "../events/publishers/ticket-created-publisher";
+import {natsWrapper} from "../nats-wrapper";
 
 
 const router = express.Router();
@@ -26,8 +27,8 @@ router.post(
             userId: req.currentUser!.id,
         });
         await ticket.save();
-        // @ts-ignore
-        await new TicketCreatedPublisher(client).publish({
+
+        await new TicketCreatedPublisher(natsWrapper.client).publish({
             id: ticket.id,
             title: ticket.title.toString(),
             price: ticket.price,
