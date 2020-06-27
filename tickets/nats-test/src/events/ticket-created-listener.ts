2d6a567f093cfaa928ba295ab70e8fa47d153c57
hawrot <mhawro200@caledonian.ac.uk>
2020-06-27 22:54:49 +0100

    Construct abstract publisher

--- tickets/nats-test/src/events/ticket-created-listener.ts
+++ tickets/nats-test/src/events/ticket-created-listener.ts
@@ -8,8 +8,10 @@ export class TicketCreatedListener extends Listener<TicketCreatedEvent>{
     readonly subject = Subjects.TicketCreated;
     queueGroupName = 'payments-service';
 
-    onMessage(data: any, msg: Message){
+    onMessage(data: TicketCreatedEvent['data'], msg: Message){
         console.log('Event data!', data);
+
+        console.log(data.title);
 
         msg.ack();
     }
