2fc024209d75e84686df9d7a8af1e65c9df43576
hawrot <mhawro200@caledonian.ac.uk>
2020-06-27 23:00:45 +0100

    Implement custom publisher into nats

--- tickets/nats-test/src/publisher.ts
+++ tickets/nats-test/src/publisher.ts
@@ -1,9 +1,9 @@
 import nats from 'node-nats-streaming';
-import { randomBytes } from "crypto";
+import {TicketCreatedPublisher} from "./events/ticket-created-publisher";
 
 console.clear();
 
-const stan = nats.connect('ticketing', randomBytes(4).toString('hex'), {
+const stan = nats.connect('ticketing', '123', {
     url: 'http://localhost:4222'
 });
 
@@ -11,7 +11,14 @@ const stan = nats.connect('ticketing', randomBytes(4).toString('hex'), {
 stan.on('connect', () => {
    console.log('Publisher connected to NATS');
 
-   const data = JSON.stringify({
+   const publisher = new TicketCreatedPublisher(stan);
+   publisher.publish({
+       id: '123',
+       title: 'concert',
+       price: 20
+   })
+
+ /*  const data = JSON.stringify({
      id: '123',
      title: 'concert',
      price: 20
@@ -19,7 +26,7 @@ stan.on('connect', () => {
 
    stan.publish('ticket:created', data, () => {
        console.log('event published');
-   })
+   })*/
 
 });
 
