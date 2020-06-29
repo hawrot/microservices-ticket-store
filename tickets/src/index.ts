d04209d4a1e345d9ed6fc6534d8a69b8f09ab784
hawrot <mhawro200@caledonian.ac.uk>
2020-06-29 22:05:25 +0100

    NATS env variables

--- tickets/src/index.ts
+++ tickets/src/index.ts
@@ -9,8 +9,17 @@ const start = async () => {
     if (!process.env.MONGO_URI) {
         throw new Error('Mongo URI must be defined')
     }
+    if (!process.env.NATS_CLIENT_ID) {
+        throw new Error('NATS_CLIENT_ID must be defined')
+    }
+    if (!process.env.NATS_URL) {
+        throw new Error('NATS_URLmust be defined')
+    }
+    if (!process.env.NATS_CLUSTER_ID) {
+        throw new Error('MNATS_CLUSTER_ID must be defined')
+    }
     try {
-        await natsWrapper.connect('ticketing', 'randomString', 'http://nats-srv:4222');
+        await natsWrapper.connect(process.env.NATS_CLUSTER_ID,process.env.NATS_CLIENT_ID, process.env.NATS_URL);
 
         natsWrapper.client.on('close', () => {
             console.log('NATS connection closed!');
