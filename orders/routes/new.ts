d1ccd3cb87ce679926d4a41c24288e18c4ebc291
hawrot <mhawro200@caledonian.ac.uk>
2020-06-29 23:43:06 +0100

    Validate the new order

--- orders/routes/new.ts
+++ orders/routes/new.ts
@@ -1,9 +1,13 @@
-
 import express, {Request, Response} from 'express';
+import {requireAuth, validateRequest} from "@mhmicrotickets/common";
+import {body} from "express-validator";
+
 
 const router = express.Router();
 
-router.post('/api/orders', async (req: Request, res: Response) => {
+router.post('/api/orders', requireAuth, [
+    body('ticketId').not().isEmpty().withMessage('Ticket id must be provided')
+], async (req: Request, res: Response) => {
     res.send({});
 });
 
