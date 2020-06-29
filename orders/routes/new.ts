a924c636e917b32df3fe9e72c343242ef88ba677
hawrot <mhawro200@caledonian.ac.uk>
2020-06-29 23:47:03 +0100

    Validate mongo id

--- orders/routes/new.ts
+++ orders/routes/new.ts
@@ -1,12 +1,13 @@
 import express, {Request, Response} from 'express';
 import {requireAuth, validateRequest} from "@mhmicrotickets/common";
 import {body} from "express-validator";
+import mongoose from 'mongoose';
 
 
 const router = express.Router();
 
 router.post('/api/orders', requireAuth, [
-    body('ticketId').not().isEmpty().withMessage('Ticket id must be provided')
+    body('ticketId').not().isEmpty().custom((input: string) => mongoose.Types.ObjectId.isValid(input)).withMessage('Ticket id must be provided')
 ], async (req: Request, res: Response) => {
     res.send({});
 });
