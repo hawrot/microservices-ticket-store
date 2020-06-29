d3a6ae6944743da570e931ab7f0fea9fecae33cd
hawrot <mhawro200@caledonian.ac.uk>
2020-06-29 23:35:58 +0100

    Configure new order router

--- orders/routes/delete.ts
+++ orders/routes/delete.ts
@@ -3,7 +3,7 @@ import express, {Request, Response} from 'express';
 
 const router = express.Router();
 
-router.delete('/api/orders:id', async (req: Request, res: Response) => {
+router.delete('/api/orders/:orderId', async (req: Request, res: Response) => {
     res.send({});
 });
 
