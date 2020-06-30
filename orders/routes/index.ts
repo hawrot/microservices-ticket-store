cdce29951aa3c94b2d1ee51d4aef1d957710a4d3
hawrot <mhawro200@caledonian.ac.uk>
2020-06-30 19:11:48 +0100

    Fetch user's orders

--- orders/routes/index.ts
+++ orders/routes/index.ts
@@ -1,10 +1,13 @@
-
 import express, {Request, Response} from 'express';
+import {requireAuth} from "@mhmicrotickets/common";
+import { Order } from "../src/models/order";
 
 const router = express.Router();
 
-router.get('/api/orders', async (req: Request, res: Response) => {
-    res.send({});
+router.get('/api/orders', requireAuth, async (req: Request, res: Response) => {
+    const orders = await Order.find({ userId: req.currentUser!.id}).populate('ticket');
+
+    res.send({orders});
 });
 
 export {router as indexOrderRouter};
