a46d84dc9f893184112ae1d978e29950b5406745
hawrot <mhawro200@caledonian.ac.uk>
2020-06-30 23:21:06 +0100

    Test order created publisher

--- orders/src/routes/__test__/new.test.ts
+++ orders/src/routes/__test__/new.test.ts
@@ -4,6 +4,7 @@ import mongoose from 'mongoose';
 import {Order} from "../../models/order";
 import {Ticket} from "../../models/ticket";
 import {OrderStatus} from "@mhmicrotickets/common";
+import {natsWrapper} from "../../nats-wrapper";
 
 it('should return an error if the ticket does not exist',  async function () {
    const ticketId = mongoose.Types.ObjectId();
@@ -52,9 +53,22 @@ it('should return a ticket',  async function () {
         .set('Cookie', global.signin())
         .send({ticketId: ticket.id})
         .expect(201)
-
-
 });
 
 
-it.todo('should emit an order created event');
+it('should  emit an order created event', async function () {
+    const ticket = Ticket.build({
+        title: 'concert',
+        price: 20
+    });
+
+    await ticket.save();
+
+    await request(app)
+        .post('/api/orders')
+        .set('Cookie', global.signin())
+        .send({ticketId: ticket.id})
+        .expect(201)
+
+    expect(natsWrapper.client.publish).toHaveBeenCalled();
+});
