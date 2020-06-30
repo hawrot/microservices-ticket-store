b3e9e92342b709b4af1e212c7e713aa5077e2ddc
hawrot <mhawro200@caledonian.ac.uk>
2020-06-30 17:59:01 +0100

    check if ticket is not reserved

--- orders/routes/new.ts
+++ orders/routes/new.ts
@@ -1,7 +1,9 @@
 import express, {Request, Response} from 'express';
-import {requireAuth, validateRequest} from "@mhmicrotickets/common";
+import {BadRequestErr, NotFoundError, OrderStatus, requireAuth, validateRequest} from "@mhmicrotickets/common";
 import {body} from "express-validator";
 import mongoose from 'mongoose';
+import {Ticket} from "../src/models/ticket";
+import {Order} from "../src/models/order";
 
 
 const router = express.Router();
@@ -9,7 +11,32 @@ const router = express.Router();
 router.post('/api/orders', requireAuth, [
     body('ticketId').not().isEmpty().custom((input: string) => mongoose.Types.ObjectId.isValid(input)).withMessage('Ticket id must be provided')
 ], validateRequest, async (req: Request, res: Response) => {
-    res.send({});
+    const {ticketId} = req.body;
+    //Find the ticket that user is trying to order
+    const ticket = await Ticket.findById(ticketId);
+
+    if (!ticket){
+        throw new NotFoundError();
+    }
+
+    //Make sure that a ticket is not already reserved
+    const existingOrder = await Order.findOne({
+        ticket:ticket,
+        status: {
+            $in: [OrderStatus.Created, OrderStatus.AwaitingPayment, OrderStatus.Complete]
+        }
+    });
+    if (existingOrder){
+        throw new BadRequestErr('Ticket is already reserved');
+    }
+
+
+    //Calculate an expiration date for an order
+
+
+    //Build the order and save into DB
+
+
 });
 
 export {router as newOrderRouter};
