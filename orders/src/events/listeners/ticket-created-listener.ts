595e0225ec92991fcb6ed8c426e054160e84367a
hawrot <mhawro200@caledonian.ac.uk>
2020-06-30 23:58:06 +0100

    Amend ticket schema to accept id value on mongoDB

--- orders/src/events/listeners/ticket-created-listener.ts
+++ orders/src/events/listeners/ticket-created-listener.ts
@@ -3,11 +3,19 @@ import {Subjects, Listener, TicketCreatedEvent} from "@mhmicrotickets/common";
 import {Ticket} from "../../models/ticket";
 import {queueGroupName} from "./queue-group-name";
 
-export class TicketCreatedListener extends Listener<TicketCreatedEvent>{
-readonly subject: Subjects.TicketCreated;
-queueGroupName =  queueGroupName;
+export class TicketCreatedListener extends Listener<TicketCreatedEvent> {
+    readonly subject = Subjects.TicketCreated;
+    queueGroupName = queueGroupName;
 
-onMessage(data: TicketCreatedEvent["data"], msg: Message) {
-}
+    async onMessage(data: TicketCreatedEvent["data"], msg: Message) {
+        const {id, title, price} = data;
+        const ticket = Ticket.build({
+            id, title, price
+        });
+
+        await ticket.save();
+
+        msg.ack();
+    }
 
 }
