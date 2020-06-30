b297531b9d0dd748f865f161f8fe90c2f04c2d98
hawrot <mhawro200@caledonian.ac.uk>
2020-06-30 19:01:45 +0100

    Improve tests

--- orders/routes/__test__/new.test.ts
+++ orders/routes/__test__/new.test.ts
@@ -1,7 +1,9 @@
 import request from 'supertest';
 import {app} from "../../src/app";
 import mongoose from 'mongoose';
-
+import {Order} from "../../src/models/order";
+import {Ticket} from "../../src/models/ticket";
+import {OrderStatus} from "@mhmicrotickets/common";
 
 it('should return an error if the ticket does not exist',  async function () {
    const ticketId = mongoose.Types.ObjectId();
@@ -16,7 +18,26 @@ it('should return an error if the ticket does not exist',  async function () {
 
 });
 it('should return an error if the ticket is already reserved',async function () {
+    const ticket = Ticket.build({
+        title: 'concert',
+        price: 20
+    });
 
+    await ticket.save();
+    const order = Order.build({
+        ticket,
+        userId: 'asdfghj',
+        status: OrderStatus.Created,
+        expiresAt: new Date()
+    })
+
+    await order.save();
+
+    await request(app)
+        .post('/api/orders')
+        .set('Cookie', global.signin())
+        .send({ticketId: ticket.id})
+        .expect(400)
 });
 it('should return a ticket',  async function () {
 
