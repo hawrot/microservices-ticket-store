22cf518ea151223471bbd7e1033f4e7d1c30eaf1
hawrot <mhawro200@caledonian.ac.uk>
2020-07-01 23:15:23 +0100

    Hook updateifCurrentPlugin

--- tickets/src/models/ticket.ts
+++ tickets/src/models/ticket.ts
@@ -1,15 +1,19 @@
 import mongoose from 'mongoose';
+import { updateIfCurrentPlugin} from "mongoose-update-if-current";
 
 interface TicketAttrs {
     title: string;
     price: number;
     userId: string;
 }
+
 interface TicketDoc extends mongoose.Document{
     title: string;
     price: number;
     userId: string;
+    version: number;
 }
+
 interface TicketModel extends mongoose.Model<TicketDoc>{
     build(attrs: TicketAttrs): TicketDoc;
 }
@@ -36,6 +40,8 @@ const ticketSchema = new mongoose.Schema({
       }
   }
 });
+ticketSchema.set('versionKey', 'version');
+ticketSchema.plugin(updateIfCurrentPlugin);
 
 ticketSchema.statics.build = (attrs: TicketAttrs) => {
     return new Ticket(attrs);
