e8e1a527edd3b3dd8cf2f25295bf4121690e824b
hawrot <mhawro200@caledonian.ac.uk>
2020-07-02 22:56:17 +0100

    Build order-created listener

--- tickets/src/events/listeners/order-created-listener.ts
+++ tickets/src/events/listeners/order-created-listener.ts
@@ -1,12 +1,25 @@
 import {Listener, OrderCreatedEvent, Subjects} from "@mhmicrotickets/common";
 import {queueGroupName} from "./quque-grouo-name";
 import {Message} from 'node-nats-streaming';
+import {Ticket} from "../../models/ticket";
 
 export class OrderCreatedListener extends Listener<OrderCreatedEvent>{
     readonly subject = Subjects.OrderCreated;
     queueGroupName = queueGroupName;
 
    async onMessage(data: OrderCreatedEvent["data"], msg: Message) {
-        
+        //Find the ticket which is reserved in by the order
+       const ticket = await Ticket.findById(data.ticket.id);
+
+       if(!ticket){
+           throw new Error('Ticket not found');
+       }
+
+       ticket.set({orderId: data.id})
+
+       await ticket.save();
+
+       msg.ack();
+
     }
 }
