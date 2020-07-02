7faab328c5589afca0b75430c62f5544379ba16d
hawrot <mhawro200@caledonian.ac.uk>
2020-07-02 16:34:20 +0100

    Hook version inside tickets routes

--- tickets/src/routes/update.ts
+++ tickets/src/routes/update.ts
@@ -1,5 +1,5 @@
 import express, {Request, Response} from "express";
-import { body } from 'express-validator';
+import {body} from 'express-validator';
 import {Ticket} from "../models/ticket";
 import {validateRequest, NotFoundError, requireAuth, NotAuthorizedError} from '@mhmicrotickets/common';
 import {TicketUpdatedPublisher} from "../events/publishers/ticket-updated-publisher";
@@ -8,40 +8,41 @@ import {natsWrapper} from "../nats-wrapper";
 
 const router = express.Router();
 
-router.put('/api/tickets/:id', requireAuth,[
-    body('title')
-        .not().isEmpty().withMessage('Title is required'),
-    body('price')
-        .isFloat({gt : 0})
-        .withMessage('Mist be provided and greater than 0'),
-],
+router.put('/api/tickets/:id', requireAuth, [
+        body('title')
+            .not().isEmpty().withMessage('Title is required'),
+        body('price')
+            .isFloat({gt: 0})
+            .withMessage('Mist be provided and greater than 0'),
+    ],
     validateRequest,
     async (req: Request, res: Response) => {
-    const ticket = await Ticket.findById(req.params.id);
+        const ticket = await Ticket.findById(req.params.id);
 
-    if(!ticket){
-        throw new NotFoundError();
-    }
+        if (!ticket) {
+            throw new NotFoundError();
+        }
 
-    if(ticket.userId !== req.currentUser!.id){
-        throw new NotAuthorizedError();
-    }
+        if (ticket.userId !== req.currentUser!.id) {
+            throw new NotAuthorizedError();
+        }
 
-    ticket.set({
-        title: req.body.title,
-        price: req.body.price
-    });
+        ticket.set({
+            title: req.body.title,
+            price: req.body.price
+        });
 
-    await ticket.save();
+        await ticket.save();
 
-    new TicketUpdatedPublisher(natsWrapper.client).publish({
-       id: ticket.id,
-       title: ticket.title,
-       price: ticket.price,
-       userId: ticket.userId
-    });
+        new TicketUpdatedPublisher(natsWrapper.client).publish({
+            id: ticket.id,
+            title: ticket.title,
+            price: ticket.price,
+            userId: ticket.userId,
+            version: ticket.version
+        });
 
-    res.send(ticket);
-})
+        res.send(ticket);
+    })
 
 export {router as updateTicketRouter};
