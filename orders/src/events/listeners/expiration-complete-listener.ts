b37f7e534704bf33c40b06276874178ed1bd34c5
hawrot <mhawro200@caledonian.ac.uk>
2020-07-03 22:52:21 +0100

    Emit order-cancel-publisher

--- orders/src/events/listeners/expiration-complete-listener.ts
+++ orders/src/events/listeners/expiration-complete-listener.ts
@@ -2,6 +2,7 @@ import { Listener, ExpirationCompleteEvent, Subjects, OrderStatus } from "@mhmic
 import {Message} from 'node-nats-streaming';
 import {queueGroupName} from "./queue-group-name";
 import {Order} from "../../models/order";
+import {OrderCancelledPublisher} from "../publishers/order-cancelled-publisher";
 
 
 export class ExpirationCompleteListener extends Listener<ExpirationCompleteEvent>{
@@ -10,16 +11,25 @@ export class ExpirationCompleteListener extends Listener<ExpirationCompleteEvent
     queueGroupName = queueGroupName;
 
     async onMessage(data: ExpirationCompleteEvent["data"], msg: Message) {
-        const order = await Order.findById(data.orderId);
+        const order = await Order.findById(data.orderId).populate('ticket');
 
         if (!order){
             throw new Error('Order not found');
         }
 
         order.set({
-            status: OrderStatus.Cancelled,
-            ticket: null
+            status: OrderStatus.Cancelled
         })
+        await order.save();
 
+       await new OrderCancelledPublisher(this.client).publish({
+            id: order.id,
+            version: order.version,
+            ticket: {
+                id: order.ticket.id
+            }
+        });
+
+       msg.ack();
     }
 }
