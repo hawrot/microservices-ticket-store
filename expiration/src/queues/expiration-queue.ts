cb915e893729a17cf7ee490837f1320798735b8f
hawrot <mhawro200@caledonian.ac.uk>
2020-07-03 22:30:54 +0100

    Hook the publisher into the queue

--- expiration/src/queues/expiration-queue.ts
+++ expiration/src/queues/expiration-queue.ts
@@ -1,4 +1,7 @@
 import Queue from 'bull';
+import {ExpirationCompleteEvent} from "../../../common/src";
+import {natsWrapper} from "../nats-wrapper";
+import {ExpirationCompletePublisher} from "../events/publishers/expiration-complete-publisher";
 
 interface Payload {
     orderId: string;
@@ -13,7 +16,9 @@ const expirationQueue = new Queue<Payload>('order:expiration', {
 });
 
 expirationQueue.process(async (job) => {
-    console.log('I want to publish an expiration:complete event for orderId', job.data.orderId);
+    await new ExpirationCompletePublisher(natsWrapper.client).publish({
+       orderId: job.data.orderId;
+    });
 });
 
 export {expirationQueue};
