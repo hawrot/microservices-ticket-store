c52c00c34a1281e099438b74cf74f4ba42d7b35e
hawrot <mhawro200@caledonian.ac.uk>
2020-07-03 21:59:20 +0100

    Build expiration-queue

--- expiration/src/events/listeners/order-created-listener.ts
+++ expiration/src/events/listeners/order-created-listener.ts
@@ -2,10 +2,11 @@ import {Listener, OrderCreatedEvent, Subjects} from "@mhmicrotickets/common";
 import {queueGroupName} from "./queue-group-name";
 import {Message} from 'node-nats-streaming';
 
-export class OrderCreatedListener extends Listener<OrderCreatedListener>{
+export class OrderCreatedListener extends Listener<OrderCreatedEvent>{
     readonly subject = Subjects.OrderCreated;
     queueGroupName = queueGroupName;
 
+    // @ts-ignore
     async onMessage(data: OrderCreatedListener["data"], msg: Message) {
     }
 }
