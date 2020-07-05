3b6cdfc3d2e192a63926d75efebe80f7e48cd91f
hawrot <mhawro200@caledonian.ac.uk>
2020-07-05 23:09:35 +0100

    Additional tests

--- payments/src/routes/__test__/new.test.ts
+++ payments/src/routes/__test__/new.test.ts
@@ -1,8 +1,8 @@
 import mongoose from 'mongoose';
 import request from 'supertest';
-import { OrderStatus } from '@mhmicrotickets/common';
-import { app } from '../../app';
-import { Order } from '../../models/order';
+import {OrderStatus} from '@mhmicrotickets/common';
+import {app} from '../../app';
+import {Order} from '../../models/order';
 
 it('returns a 404 when purchasing an order that does not exist', async () => {
     await request(app)
@@ -35,4 +35,25 @@ it('returns a 401 when purchasing an order that doesnt belong to the user', asyn
         .expect(401);
 });
 
-it('returns a 400 when purchasing a cancelled order', async () => {});
+it('returns a 400 when purchasing a cancelled order', async () => {
+    const userId = mongoose.Types.ObjectId().toHexString();
+
+    const order = Order.build({
+        id: mongoose.Types.ObjectId().toHexString(),
+        userId: userId,
+        version: 0,
+        price: 20,
+        status: OrderStatus.Cancelled,
+    });
+    await order.save();
+
+    await request(app)
+        .post('/api/payments')
+        .set('Cookie', global.signin(userId))
+        .send({
+            orderId: order.id,
+            token: 'asdaasd'
+        })
+        .expect(400);
+
+});
