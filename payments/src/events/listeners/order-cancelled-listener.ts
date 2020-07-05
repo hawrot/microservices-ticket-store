4438ecb625a5506df0b2db55fe0b63435bb26a4a
hawrot <mhawro200@caledonian.ac.uk>
2020-07-05 22:32:53 +0100

    Fix the listener

--- payments/src/events/listeners/order-cancelled-listener.ts
+++ payments/src/events/listeners/order-cancelled-listener.ts
@@ -1,24 +1,28 @@
-import {OrderCancelledEvent, Subjects, Listener, OrderStatus} from "@mhmicrotickets/common";
-import {queueGroupName} from "./queue-group-name";
-import {Message} from 'node-nats-streaming';
-import {Order} from '../../models/order';
+import {
+    OrderCancelledEvent,
+    Subjects,
+    Listener,
+    OrderStatus,
+} from '@mhmicrotickets/common';
+import { Message } from 'node-nats-streaming';
+import { queueGroupName } from './queue-group-name';
+import { Order } from '../../models/order';
 
 export class OrderCancelledListener extends Listener<OrderCancelledEvent> {
-    readonly subject = Subjects.OrderCancelled;
+    subject: Subjects.OrderCancelled = Subjects.OrderCancelled;
     queueGroupName = queueGroupName;
 
-    async onMessage(data: OrderCancelledEvent["data"], msg: Message) {
+    async onMessage(data: OrderCancelledEvent['data'], msg: Message) {
         const order = await Order.findOne({
             _id: data.id,
-            version: data.version = 1
+            version: data.version - 1,
         });
 
         if (!order) {
             throw new Error('Order not found');
         }
 
-        order.set({status: OrderStatus.Cancelled});
-
+        order.set({ status: OrderStatus.Cancelled });
         await order.save();
 
         msg.ack();
