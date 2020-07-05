7a0f684fe97f66c9dddf2a71fae18d8e6f894cd5
hawrot <mhawro200@caledonian.ac.uk>
2020-07-05 22:50:49 +0100

    Build router for a new payment

---
+++ payments/src/routes/new.ts
@@ -0,0 +1,15 @@
+import express, {Request, Response} from 'express';
+import {body} from "express-validator";
+import {requireAuth, validateRequest, BadRequestErr, NotFoundError} from "@mhmicrotickets/common";
+import {Order} from "../models/order";
+
+const router = express.Router();
+
+router.post('/api/payments', requireAuth, [
+    body('token').not().isEmpty(),
+    body('orderId').not().isEmpty()
+], (req: Request, res: Response) => {
+    res.send({success: true})
+})
+
+export {router as createChargeRouter};
