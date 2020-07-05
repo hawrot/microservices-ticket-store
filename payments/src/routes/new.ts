2dafd338c0a8e09889bc84f73e99213edf937bd8
hawrot <mhawro200@caledonian.ac.uk>
2020-07-05 22:58:46 +0100

    Configure the new payment route

--- payments/src/routes/new.ts
+++ payments/src/routes/new.ts
@@ -1,6 +1,13 @@
-import express, {Request, Response} from 'express';
+import express, {raw, Request, Response} from 'express';
 import {body} from "express-validator";
-import {requireAuth, validateRequest, BadRequestErr, NotFoundError} from "@mhmicrotickets/common";
+import {
+    requireAuth,
+    validateRequest,
+    BadRequestErr,
+    NotFoundError,
+    NotAuthorizedError,
+    OrderStatus
+} from "@mhmicrotickets/common";
 import {Order} from "../models/order";
 
 const router = express.Router();
@@ -8,8 +15,24 @@ const router = express.Router();
 router.post('/api/payments', requireAuth, [
     body('token').not().isEmpty(),
     body('orderId').not().isEmpty()
-], validateRequest, (req: Request, res: Response) => {
-    res.send({success: true})
+], validateRequest, async (req: Request, res: Response) => {
+    const {token, orderId} = req.body;
+
+    const order = await Order.findById(orderId);
+
+    if (!order) {
+        throw new NotFoundError();
+    }
+
+    if (order.userId !== req.currentUser!.id) {
+        throw new NotAuthorizedError();
+    }
+
+    if (order.status === OrderStatus.Cancelled) {
+        throw new BadRequestErr('Cannot pay for this order');
+    }
+
+    res.send({success: true});
 })
 
 export {router as createChargeRouter};
