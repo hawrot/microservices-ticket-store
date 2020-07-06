bdde282110cfa6a159a46a9ff4a011b244d1fbf5
hawrot <mhawro200@caledonian.ac.uk>
2020-07-07 00:26:05 +0100

    Post req to API

--- client/pages/tickets/new.js
+++ client/pages/tickets/new.js
@@ -1,32 +1,47 @@
 import {useState} from 'react';
+import useRequest from '../../hooks/use-request';
 
 const NewTicket = () => {
 
 
     const [title, setTitle] = useState('');
     const [price, setPrice] = useState('');
+    const {doRequest, errors} = useRequest({
+        url: '/api/tickets',
+        method: 'post',
+        body: {
+            title, price
+        },
+        onSuccess: (ticket) => console.log(ticket)
+    });
+
+    const onSubmit = (event) => {
+        event.preventDefault();
+        doRequest();
+    }
 
     const onBlur = () => {
-      const value = parseFloat(price);
+        const value = parseFloat(price);
 
-      if (isNaN(value)){
-          return;
-      }
+        if (isNaN(value)) {
+            return;
+        }
 
-      setPrice(value.toFixed(2));
+        setPrice(value.toFixed(2));
     };
 
 
     return <div>
         <h1>Create a ticket</h1>
-        <form>
+        <form onSubmit={onSubmit}>
             <div className="form-group">
                 <label>Title</label>
                 <input value={title} onChange={(e) => setTitle(e.target.value)} className="form-control"/>
             </div>
             <div className="form-group">
                 <label>Price</label>
-                <input onBlur={onBlur} value={price} onChange={(e) => setPrice(e.target.value)} className="form-control"/>
+                <input onBlur={onBlur} value={price} onChange={(e) => setPrice(e.target.value)}
+                       className="form-control"/>
             </div>
             <button className="btn btn-primary">Submit</button>
         </form>
