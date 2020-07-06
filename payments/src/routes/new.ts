9dfe00d9a4ff8eaa80d80791aaa46ed6f4fbcae2
hawrot <mhawro200@caledonian.ac.uk>
2020-07-06 23:09:50 +0100

    Create payment created listener

--- payments/src/routes/new.ts
+++ payments/src/routes/new.ts
@@ -20,7 +20,7 @@ router.post('/api/payments', requireAuth, [
     body('token').not().isEmpty(),
     body('orderId').not().isEmpty()
 ], validateRequest, async (req: Request, res: Response) => {
-    const { token, orderId } = req.body;
+    const {token, orderId} = req.body;
 
     const order = await Order.findById(orderId);
 
@@ -45,13 +45,13 @@ router.post('/api/payments', requireAuth, [
     });
     await payment.save();
 
-   await new PaymentCreatedPublisher(natsWrapper.client).publish({
-       id: payment.id,
-       orderId: payment.orderId,
-       stripeId: payment.stripeId
+    await new PaymentCreatedPublisher(natsWrapper.client).publish({
+        id: payment.id,
+        orderId: payment.orderId,
+        stripeId: payment.stripeId
     });
 
-    res.status(201).send({ success: true });
+    res.status(201).send({id: payment.id});
 })
 
 export {router as createChargeRouter};
