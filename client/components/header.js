23aab86e89506149b83b72a24dd1072b6fac0f9b
hawrot <mhawro200@caledonian.ac.uk>
2020-07-07 19:42:52 +0100

    Add navigation for new ticket and orders

--- client/components/header.js
+++ client/components/header.js
@@ -5,6 +5,8 @@ export default ({currentUser}) => {
     const links = [
         !currentUser && {label: 'Sign up', href: '/auth/signup'},
         !currentUser && {label: 'Sign in', href: '/auth/signin'},
+        currentUser && {label: 'Sell Tickets', href: '/tickets/new'},
+        currentUser && {label: 'Orders', href: '/orders'},
         currentUser && {label: 'Sign out', href: '/auth/signout'},
     ].filter(linkConfig => linkConfig)
         .map(({label, href}) => {
